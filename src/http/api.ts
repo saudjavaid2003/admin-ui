eb4007f271c262572b1905a535e5258aa6d7e5e2
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-03-30 22:09:57 +0500

    fixing configuratiosn of kong oss

--- src/http/api.ts
+++ src/http/api.ts
@@ -12,14 +12,14 @@ export const login = (credentials: Credentials) =>
 export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
 export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
 // export const getUsers=()=> api.get(`${AUTH_SERVICE}/auth/users`);
-export const getUsers = (queryString: string) => api.get(`${AUTH_SERVICE}/auth/users?${queryString}`);
+export const getUsers = (queryString: string) => api.get(`${AUTH_SERVICE}/users?${queryString}`);
 export const getTenants = (queryString: string) =>
-    api.get(`${AUTH_SERVICE}/auth/tenants?${queryString}`);
-export const createUser = (user: CreateUserData) => api.post(`${AUTH_SERVICE}/auth/users`, user);
+    api.get(`${AUTH_SERVICE}/tenants?${queryString}`);
+export const createUser = (user: CreateUserData) => api.post(`${AUTH_SERVICE}/users`, user);
 export const createTenant = (tenant: CreateTenantData) =>
-    api.post(`${AUTH_SERVICE}/auth/tenants`, tenant);
+    api.post(`${AUTH_SERVICE}/tenants`, tenant);
 export const updateUser = (user: CreateUserData, id: string) =>
-    api.patch(`${AUTH_SERVICE}/auth/users/${id}`, user);
+    api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 
 // // Catelog service
 // export const getCategories = () => api.get(`${CATALOG_SERVICE}/categories`);
